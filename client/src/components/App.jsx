e76b342d70d28d5fbd9ebeefdc58a41f46897875
fermi_dirac <ManueleBryan@gmail.com>
2018-02-03 16:49:32 -0800

    fixed front end. server up and running

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,46 @@
+import React from 'react';
+
 class App extends React.Component {
-  
+
   constructor(props) {
     super(props);
+
+    this.state = {
+      response: '',
+    };
   }
+
+
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
 
   render() {
     return(
       <div>
       DocuGenXtreme
+      <br/>
+      <br/>
+      <br/>
+      API Call example:
+      <br/>
+      {this.state.response}
       </div>
     );
   }
 
 }
+
+export default App;
