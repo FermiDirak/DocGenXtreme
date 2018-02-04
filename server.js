59f0d97608a7863c2d2a1e83173faa60beb15f93
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-02-03 17:41:03 -0800

    new server

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 
-const app = express();
-const port = process.env.PORT || 5000;
+//app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+app.use(express.static('client'))
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
