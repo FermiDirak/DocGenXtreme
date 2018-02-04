168d0775b3f760962d6c4ed90e5209c8eaedbfda
Nyah Check <check.nyah@gmail.com>
2018-02-03 16:37:37 -0800

    Added basic render test for App.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+/*
+ * Data
+ */
+
+var contacts = [
+  {key: 1, name: "James K Nelson", email: "james@jamesknelson.com", description: "Front-end Unicorn", companyName: "MocuSign", address: "944 Market Street, San Francisco, CA. 94115"},
+  {key: 2, name: "Jim", email: "jim@example.com", companyName: "MocuSign", address: "944 Market Street, San Francisco, CA. 94115"},
+  {key: 3, name: "Joe"},
+]
+
+var newContact = {name: "", email: "", description: ""}
+
+
+/*
+ * Entry point
+ */
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App />,
+    React.createElement(ContactView, {
+      contacts: contacts,
+      newContact: newContact
+    }));
   ReactDOM.unmountComponentAtNode(div);
 });
