5925e8cea2931e8c5ddabed8ec907550102e2e5b
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-02-03 17:50:01 -0800

    trying server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express()
 
 //app.get('/', (req, res) => res.send('Hello World!'))
 
-app.use(express.static('client'))
+app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
