fb516855a693ee29240622f3273e1b0674f71551
Hack Reactor Student <student@hackreactor.com>
2018-02-03 17:38:12 -0800

    added logo image

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,8 @@
 var NavBar = () => (
   <div>
-        <nav className="navbar title">
-          <div className="col-md-6 offset-md-3">
-            <h2>MocuSign</h2>
-          </div>
-        </nav>
+    <nav className="navbar title">
+        <img src="imgs/mocusign_logo.png" id="logo"/>
+    </nav>
   </div>
 );
 
